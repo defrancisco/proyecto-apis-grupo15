1122649b599dc19d6c7167d8368645860f3a2ddc
iopatich <iopatich@uade.edu.ar>
2024-10-14 17:44:43 -0300

    Card juegos

--- src/componentes/Catalogo/Card.jsx
+++ src/componentes/Catalogo/Card.jsx
@@ -3,10 +3,12 @@ import './Card.css';
 
 export default function Card(props) {
   return (
-    <div className="Card">
-      <img src={props.image} alt={props.title} />
-      <p>{props.title}</p>
-      <p>{props.price}</p>
+    <div className="Cards">
+      <div className="Card">
+        <img src={props.image} alt={props.title} />
+        <p>{props.title}</p>
+        <p>{props.price}</p>
+      </div>
     </div>
   )
 }
