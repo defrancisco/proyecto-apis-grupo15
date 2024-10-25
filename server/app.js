54c7af054a5cd56ab1d89475548611b8f5eb2df6
delfina <delfina.franciscofrate@gmail.com>
2024-10-25 12:38:35 -0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,13 +4,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Para manejar JSON en las solicitudes
 
-
 app.get('/', (req, res) => {
   res.send('Backend');
 });
 
-
-
-app.listen(port, () => {
+app.listen(PORT, () => {  // Cambié 'port' por 'PORT' aquí
   console.log(`App listening at http://localhost:${PORT}`);
 });
