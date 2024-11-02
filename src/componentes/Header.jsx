554185141decaac2cd060e7d1da78bb93c674d10
delfina <delfina.franciscofrate@gmail.com>
2024-11-02 20:39:13 -0300

    Front Ruteo y Modificaciones Header y Footer

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -3,41 +3,52 @@ import { Link } from 'react-router-dom';
 import '../styles/headeryfooter.css'; 
 import logo from './imagenes/logo.png';
 import shoppingcart from './imagenes/shoppingcart.jpg';
+import profile from './imagenes/profile.png';
+import { useUser } from '../routes/root';
 
 function Header() {
+  const { userType } = useUser() || {}; // Manejo de undefined
+
   return (
     <header>
       <div className="header-container">
         <div className="container-left">
-          <Link to="/preinicio" className="logo">
+          <Link to="/prePagina" className="logo">
             <img src={logo} alt="Nintendo Logo" />
           </Link>
 
           <nav>
             <ul>
-              <li><Link to="/preinicio">Inicio</Link></li>
+              <li><Link to="/prePagina">Inicio</Link></li>
               <li><Link to="/catalogo">Catálogo</Link></li>
-              <li><Link to="/wishlist">Wishlist</Link></li>
+              <li><Link to={userType ? "/wishlist" : "/iniciarSesion/loginUsuario"}>
+              Wishlist</Link></li>
               <li><Link to="/consolas">Consolas</Link></li>
-              <li><Link to="/quienes-somos">Quienes somos</Link></li>
-              <li><Link to="/perfil">Perfil</Link></li>
+              <li><Link to="/quienesSomos">Quienes somos</Link></li>
               <li><Link to="/ayuda">Ayuda</Link></li>
             </ul>
           </nav>
         </div>
 
         <div className="auth-buttons">
-          <Link to="/usuario/login">
-            <button id="login-btn">Iniciar sesión</button>
-          </Link>
+          <li>
+                {userType ? (
+                  <Link to={userType === 'business' ? '/empresa/bussinessTab' : '/usuario/UserTab'}>
+                    <img src={profile} alt="Perfil" /> 
+                  </Link>
+                ) : (
+                  <Link to="/usuario/login">
+                    <button id="login-btn">Iniciar sesión</button>
+                  </Link>
+                )}
+              </li>
           <Link to="/usuario/registro">
             <button id="register-btn">Registrarse</button>
           </Link>
-          <Link to="/preinicio" className="logo">
+          <Link to="/carritoCompra" className="logo">
             <img src={shoppingcart} alt="Cart Logo" />
           </Link>
         </div>
-        
       </div>
     </header>
   );
