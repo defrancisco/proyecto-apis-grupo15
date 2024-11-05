65baaa89e0a182dbb4c5daea88e68ac29e846df2
Antonioo-Wu <anwu@uade.edu.ar>
2024-11-05 02:37:31 -0300

    db

--- server/config/database.js
+++ server/config/database.js
@@ -1,21 +1,53 @@
-// config/database.js
 const { Sequelize } = require('sequelize');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
-const sequelize = new Sequelize('nombre_base_datos', 'CloudSA1fb8f7cc', 'contraseña', {
-  host: 'sv-nintendo-api.database.windows.net', // Cambia al host de tu servidor SQL de Azure
+// Verificar las variables  
+console.log('Verificando variables de entorno:', {
+  DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PORT: process.env.DB_PORT
+});
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mssql',
   dialectOptions: {
     options: {
-      encrypt: true, // Azure requiere conexión encriptada
-      trustServerCertificate: false
+      encrypt: true,
+      trustServerCertificate: false,
+      enableArithAbort: true
     }
   },
-  port: 1433 // Asegúrate de usar el puerto correcto
+  logging: false 
 });
 
+// TEST CONEXION 
 sequelize
   .authenticate()
-  .then(() => console.log('Connected to SQL Server on Azure'))
-  .catch(err => console.error('Error connecting to SQL Server on Azure:', err));
+  .then(() => console.log('SIUUUU'))
+  .catch(err => console.error('NOOOOO', err));
+
+// ver las tablas BORRAR
+async function mostrarTablas() {
+  try {
+    const tablas = await sequelize.showAllSchemas();
+    console.log('Tablas en la base de datos:', tablas);
+    
+    for (const tabla of tablas) {
+      const estructura = await sequelize.query(`DESCRIBE ${tabla.name}`);
+      console.log(`Estructura de ${tabla.name}:`, estructura);
+    }
+  } catch (error) {
+    console.error('Error al obtener tablas:', error);
+  }
+}
+
+mostrarTablas();
 
 module.exports = sequelize;
