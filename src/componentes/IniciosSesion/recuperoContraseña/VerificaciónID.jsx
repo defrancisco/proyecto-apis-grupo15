7939ba2462df1d799b9d7e5c3052b3eae99d76fe
Ignacio <iopatich@uade.edu.ar>
2024-11-08 11:46:24 -0300

    Correcciones 🤨😝

--- src/componentes/IniciosSesion/recuperoContraseña/VerificaciónID.jsx
+++ src/componentes/IniciosSesion/recuperoContraseña/VerificaciónID.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from './AuthContext';
+import { useAuth } from '../../../routes/AuthContext';
 import '../../../styles/usuario/verificacionID.css';
 
 const VerificacionIdentidad = ({ code, setCode, onSubmit, onResend }) => {
--- src/componentes/extras/ContactForm.jsx
+++ src/componentes/extras/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './ContactForm.css';
+import '../../styles/contactForm.css';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
