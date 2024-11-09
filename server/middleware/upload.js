18069eb11d5a99d7f59d1454a2bcc9725662fb4c
Antonioo-Wu <anwu@uade.edu.ar>
2024-11-09 14:19:56 -0300

    blob

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,14 +1,6 @@
 const multer = require('multer');
-const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
+const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
   const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
